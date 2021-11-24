4ed39639f88384abe176b5bb0996a5f13b6bfdb6
Andrey Prisniiak <andrey.prisniak@gmail.com>
2021-11-24 17:05:24 +0000

    Update src/api/config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,5 @@
 // Config
-
-const isProduction = process.env.NODE_ENV === 'production';
-
 export const ROOT_URL = process.env.REACT_APP_API_URL;
-export const FEED_URL = isProduction ? `${ROOT_URL}/rtx/api/feed` : `${ROOT_URL}/feed`;
-export const AUTH_URL = isProduction ? `${ROOT_URL}/rtx/api/live` : `${ROOT_URL}/live`;
+
+export const FEED_URL = `${ROOT_URL}/rtx/api/feed`;
+export const AUTH_URL = `${ROOT_URL}/rtx/api/live`;
